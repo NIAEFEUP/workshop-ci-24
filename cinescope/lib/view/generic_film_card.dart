269f50e29173e919487c432f63d03c5ed0704982
Francisco Ana <franciscopdana@gmail.com>
2023-04-15 23:09:15 +0100

    open movie page by clicking on its card

--- cinescope/lib/view/generic_film_card.dart
+++ cinescope/lib/view/generic_film_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cinescope/model/film.dart';
+import 'package:cinescope/view/pages/film_page.dart';
 
 class GenericFilmCard extends StatelessWidget {
   final Film film;
@@ -8,42 +9,50 @@ class GenericFilmCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
-            film.imgUrl,
-            width: 120,
-            height: 160,
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  film.title,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  film.type,
-                  style: const TextStyle(fontSize: 14),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  film.year.toString(),
-                  style: const TextStyle(fontSize: 14),
-                ),
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FilmPage(film.id)),
+        );
+      },
+      child: Card(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              film.imgUrl,
+              width: 120,
+              height: 160,
+              fit: BoxFit.cover,
             ),
-          ),
-        ],
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    film.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    film.type,
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    film.year.toString(),
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
